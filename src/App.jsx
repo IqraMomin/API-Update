4801dddca4d9e72897b139ff67e2df2c9f53e046
IqraMomin <iqra2018momin@gmail.com>
2025-10-28 04:57:08 +0530

    APIUpdate

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import './App.css'
+import MovieList from './component/MovieList'
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      const transformedData = data.results.map(movie => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date
+        };
+      });
+
+      setMovies(transformedData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MovieList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/component/MovieList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Movie from './Movie'
+import "./MovieList.css"
+
+function MovieList(props) {
+    return (
+        <ul className='movies-list'>
+            {props.movies.map(movie=>{
+                return <Movie
+                title={movie.title}
+                openingText={movie.openingText}
+                releaseDate={movie.releaseDate}/>
+            })}
+        </ul>
+        
+    )
+}
+
+export default MovieList
