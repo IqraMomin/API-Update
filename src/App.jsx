eeec9b617747e098c9dded0bfe63f67b26e61aa8
IqraMomin <iqra2018momin@gmail.com>
2025-10-28 05:19:09 +0530

    APIUpdate2

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,36 @@ import './App.css'
 import MovieList from './component/MovieList'
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const fetchMoviesHandler = async () => {
-    try {
+  
+  const [movies,setMovies] = useState([]);
+  const fetchMoviesHandler = async ()=>{
+    try{
       const response = await fetch("https://swapi.dev/api/films/");
-      if (!response.ok) {
-        throw new Error("Something went wrong!");
-      }
-
       const data = await response.json();
-
-      const transformedData = data.results.map(movie => {
+      const transformedData = data.results.map(movie=>{
         return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date
-        };
-      });
-
+          id:movie.episode_id,
+          title:movie.title,
+          openingText:movie.opening_crawl,
+          releaseDate:movie.release_date
+        }
+      })
       setMovies(transformedData);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+      
+     }catch(err){
 
+    }
+  }
   return (
     <React.Fragment>
-      <section>
+      <section className='btn-section'>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MovieList movies={movies} />
+        <MovieList movies={movies}/>
       </section>
     </React.Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
