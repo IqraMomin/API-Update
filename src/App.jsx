04c05023488c4d078c9b68a0b1027b198e7f32e9
IqraMomin <iqra2018momin@gmail.com>
2025-10-28 20:33:23 +0530

    ErrorHandling

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ function App() {
   
   const [movies,setMovies] = useState([]);
   const [isLoading,setIsLoading] = useState(false);
+  const [error,setError] = useState(null);
   const fetchMoviesHandler = async ()=>{
+    setError(null);
     setIsLoading(true);
     try{
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://swapi.dev/api/film/");
+      if(!response.ok){
+        throw new Error("Something went wrong...Retrying");
+      }
       const data = await response.json();
       const transformedData = data.results.map(movie=>{
         return {
@@ -20,11 +25,13 @@ function App() {
         }
       })
       setMovies(transformedData);
-      setIsLoading(false);
+      setError(null);
       
      }catch(err){
+      setError(err.message);
 
     }
+    setIsLoading(false);
   }
   return (
     <React.Fragment>
@@ -32,8 +39,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-       {!isLoading && <MovieList movies={movies}/>}
+       {!isLoading && movies.length>0 && <MovieList movies={movies}/>}
        {isLoading && <p>Loading...</p>}
+       {!isLoading && movies.length===0 && !error && <p>No movies Found</p>}
+       {error && <p>{error}</p>}
       </section>
     </React.Fragment>
   )
